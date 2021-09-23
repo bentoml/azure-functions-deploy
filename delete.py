6437cb38e26172e6fc25cd5b6ef6dc4423f628a8
rajeshkumarrs <rs_rajesh_kumar@yahoo.co.in>
2021-09-23 09:01:41 +0530

    Commit after rebase - Modified README.md to include a step to update azure_config.json,azure_config.json to add a indcative message to update "location",delete.py deploy.py and update.py to include argparse and rich format

--- delete.py
+++ delete.py
@@ -1,20 +1,32 @@
 import sys
+import os
+import argparse
+from rich.pretty import pprint
 
 from utils import run_shell_command
 
-from azure import generate_resource_names
+from azurefunctions import generate_resource_names
 
 
-def delete_azure(deployment_name):
-    resource_group_name, _, _, _, _ = generate_resource_names(deployment_name)
+def delete(app_name):
+    print(f"Deleting {app_name}")
+    resource_group_name, _, _, _, _ = generate_resource_names(app_name)
     run_shell_command(["az", "group", "delete", "-y", "--name", resource_group_name])
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        raise Exception(
-            "Please provide deployment_name"
-        )
-    deployment_name = sys.argv[1]
+    parser = argparse.ArgumentParser(
+        prog='delete',
+        description="Delete the bentoml bundle on Azure Functions",
+        epilog="Check out https://github.com/bentoml/azure-functions-deploy/blob/main/README.md to know more",
+    )
 
-    delete_azure(deployment_name)
+    parser.add_argument(
+        "app_name", help="Name of the App to be deleted"
+    )
+
+    args = parser.parse_args()
+
+    delete(args.app_name)
+
+    pprint(f"Deleted {args.app_name}!")
